fd88595cd085e07a2e123c429501f1093d49bc2d
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2018-12-19 17:38:11 +0900

    Make post model

--- library-appli/db/schema.rb
+++ library-appli/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181219081451) do
+ActiveRecord::Schema.define(version: 20181219082536) do
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "quote",      limit: 65535, null: false
+    t.text     "comment",    limit: 65535
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
