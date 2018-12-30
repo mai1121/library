1e0b98bd78e4dce1064f55f71dd76140dafeff50
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2018-12-31 00:28:01 +0900

    edit posts page

--- library-appli/app/controllers/posts_controller.rb
+++ library-appli/app/controllers/posts_controller.rb
@@ -1,20 +1,14 @@
 class PostsController < ApplicationController
 	def index
-		# @items = RakutenWebService::Ichiba::Item.search(keyword: "海辺のカフカ 村上春樹 ")
-	  # images_arr = []
-	  # items.each do |item|
-
-	  # # したい処理
-	  # @title = item['itemName'] #商品名
-	  # @price = item['itemPrice'] #価格
-	  # @url = item['itemUrl'] #商品のURL
-	  # @image = item['smallImageUrl']
-	  # end
 	end
 	def show
-
-		
+	
 	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
 	def new
 		@post = Post.new
 	end
