0b0c798a9afdbce2015199bad0538bea9ba90905
mai1121 <maiukawa@maiukawanoMacBook-ea.local>
2018-12-30 20:34:21 +0900

    Edit mordal window

--- library-appli/app/controllers/users_controller.rb
+++ library-appli/app/controllers/users_controller.rb
@@ -3,27 +3,33 @@ class UsersController < ApplicationController
 	  	@users = User.all
 	end
 	def show
-		user = User.find(params[:id])
-		@nickname = user.nickname
-        @posts = user.posts
+		@user = User.find(params[:id])
+		@nickname = @user.nickname
+        @posts = @user.posts
         # 楽天より書籍情報を検索
-        # ユーザーの投稿のタイトルを配列形式で格納
-        # keywords = user.posts.title
         # # タイトルの配列を引数にして検索メソッドを呼び出し
-        @item = search_rakuten_api("海辺のカフカ")  
+        @items = search_rakuten_api(@posts)  
 	end
 
 
-	def search_rakuten_api(keyword)
+	def search_rakuten_api(keywords)
 		# 検索結果格納する空の配列を定義
-		# search_items = []
-		# keywords.each do |keyword|
-			item = RakutenWebService::Books::Book.search(title: keyword).first
-			item_image = item['smallImageUrl']
-			# search_items << item
-		# end
+		search_items = []
+		keywords.each do |keyword|
+			item = RakutenWebService::Books::Book.search(title: keyword[:title],author: keyword[:author]).first
+      # binding.pry
+      if item.nil? 
+        item_image = ""
+        item_url = ""
+      else
+			  item_image = item['mediumImageUrl']
+        item_url = item['itemUrl']
+      end
+			search_items << {image: item_image, url: item_url}
+		end
+    
 		# 配列を返す
-		# return search_items
+		return search_items
 	end
 
 end
